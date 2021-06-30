4fd7878d79cf0625faeb3581634de41d1b9d1a2a
Aimee Maroney <ammarone@syr.edu>
2021-06-30 16:53:29 -0400

    add image to article

--- components/Article.vue
+++ components/Article.vue
@@ -3,6 +3,7 @@
     v-editable="blok"
   >
     <h1>{{ blok.title }}</h1>
+    <img :src="blok.feature_image.filename" alt="" />
     <p>{{ blok.intro}}</p>
     <rich-text-renderer
       :document="blok.body_text"
