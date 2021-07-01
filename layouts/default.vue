baa325a93c0ac2cc5b37b65b47277de0c6a60dd2
Aimee Maroney <ammarone@syr.edu>
2021-06-30 21:01:44 -0400

    add footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,15 +2,18 @@
   <div>
     <SiteHeader />
     <Nuxt />
+    <SiteFooter />
   </div>
 </template>
 
 <script>
 import SiteHeader from '../components/layout/SiteHeader.vue';
+import SiteFooter from '../components/layout/SiteFooter.vue';
 
 export default {
   components: {
     SiteHeader,
+    SiteFooter,
   }
 }
 </script>
