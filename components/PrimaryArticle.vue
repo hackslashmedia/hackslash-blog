034ed106fca665e36427969a881d7d4e011deb35
Aimee Maroney <ammarone@syr.edu>
2021-07-07 11:46:05 -0400

    add primary article

--- components/PrimaryArticle.vue
+++ components/PrimaryArticle.vue
@@ -1,3 +1,41 @@
 <template>
-    <div>Test</div>
+    <div v-editable="blok" class="primary-article">
+        <h2>{{ blok.title }}</h2>
+        <div>
+      <div v-for="article in primaryArticle" :key="article._uid">
+        <article-teaser
+          v-if="article.content"
+          :article-link="article.full_slug"
+          :article-image="article.content"
+          :article-content="article.content"/>
+        <p v-else>This content loads on save. <strong>Save the entry & reload.</strong></p>
+      </div>
+    </div>
+    </div>
 </template>
+
+<script>
+export default {
+    props: {
+        blok: {
+            type: Object,
+            required: true
+        },
+    },
+    computed: {
+    sortedArticles() {
+      // Load reference data/content from store
+      const primaryArticle = this.$store.state.articles.articles.filter((article) => {
+        return this.blok.articles.includes(article.uuid)
+      })
+ 
+      // Enable the ordering of the article previews
+      primaryArticle.sort((a, b) => {
+        return this.blok.articles.indexOf(a.uuid) - this.blok.articles.indexOf(b.uuid);
+      })
+ 
+      return primaryArticle
+    }
+  }
+};
+</script>
--- plugins/components.js
+++ plugins/components.js
@@ -3,6 +3,7 @@ import Page from '~/components/Page.vue'
 import Grid from '~/components/Grid.vue'
 import Feature from '~/components/Feature.vue'
 import FeaturedArticles from '~/components/FeaturedArticles.vue'
+import PrimaryArticle from '~/components/PrimaryArticle.vue'
 import ArticleTeaser from '~/components/ArticleTeaser.vue'
  
 Vue.component('page', Page)
@@ -10,3 +11,4 @@ Vue.component('grid', Grid)
 Vue.component('feature', Feature)
 Vue.component('featured-articles', FeaturedArticles)
 Vue.component('article-teaser', ArticleTeaser)
+Vue.component('primary-article', PrimaryArticle)
